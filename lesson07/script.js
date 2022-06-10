839589ec6a834af3a2cb35d068cc0954050468c6
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-10 07:40:17 +0200

    added a more difficult task of lesson 7

---
+++ lesson07/script.js
@@ -0,0 +1,24 @@
+'use strict';
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const day = document.getElementById('fordays'); 
+const todayDay = new Date();
+
+const days = () => {
+  week.forEach((item, i) => {
+    let newdiv = document.createElement('div');
+    if (i === +todayDay.getDay() - 1) { 
+      console.log(todayDay.getDay());
+      newdiv.classList.add('today');
+      newdiv.textContent = week[i];
+    }
+    if (item == 'Суббота' || item == 'Воскресенье') { 
+      newdiv.classList.add('italic'); 
+      newdiv.textContent = week[i];
+    } else {
+      newdiv.textContent = week[i];
+    }
+    day.appendChild(newdiv);
+
+  });
+};
+days(); 
